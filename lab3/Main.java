c50341ff42b2c0fd50a794e0bbc213ae8df2e6ef
Dmytro Fedkiv <dmytro.fedkiv@icloud.com>
2026-04-09 01:31:56 -0400

    lab-3: Complete laboratory 3

---
+++ lab3/Main.java
@@ -0,0 +1,67 @@
+public class Main {
+  public static void main(String[] args) {
+    String text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+    String bestCase = "A";
+
+    // * Initialize startTime here
+    long bestCaseStartTime = System.nanoTime();
+
+    int index = match(text, bestCase);
+
+    // * Initialize endTime here
+    long bestCaseEndTime = System.nanoTime();
+
+    // * Calculate elapsedTime here
+    long bestCaseElapsedTime = bestCaseEndTime - bestCaseStartTime;
+
+    if (index >= 0)
+      System.out.println("best-case input matched at index " + index);
+    else
+      System.out.println("best-case input unmatched");
+
+    System.out.println("best-case elapsed time: " + bestCaseElapsedTime + " ns");
+
+    String worstCase = "9";
+
+    // * Initialize startTime here
+    long worstCaseStartTime = System.nanoTime();
+
+    index = match(text, worstCase);
+
+    // * Initialize endTime here
+    long worstCaseEndTime = System.nanoTime();
+
+    // * Calculate elapsedTime here
+    long worstCaseElapsedTime = worstCaseEndTime - worstCaseStartTime;
+
+    if (index >= 0)
+      System.out.println("worst-case input matched at index " + index);
+    else
+      System.out.println("worst-case input unmatched");
+
+    System.out.println("worst-case elapsed time: " + worstCaseElapsedTime + " ns");
+  }
+
+  // Return the index of the first match. -1 otherwise.
+  public static int match(String text, String pattern) {
+    for (int i = 0; i < text.length() - pattern.length() + 1; i++) {
+      if (isMatched(i, text, pattern))
+        return i;
+    }
+
+    return -1;
+  }
+
+  // Test if pattern matches text starting at index i
+  private static boolean isMatched(int i, String text,
+      String pattern) {
+    for (int k = 0; k < pattern.length(); k++) {
+      if (pattern.charAt(k) != text.charAt(i + k)) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
